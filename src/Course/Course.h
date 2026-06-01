eb5751739d31039eaa5132b17847c7d3119ddf08
Spoof <aamir.jailbreak@gmail.com>
2026-06-01 23:47:02 +0500

    i hab tried my best to wrtie a logic

---
+++ src/Course/Course.h
@@ -0,0 +1,52 @@
+#ifndef COURSE_H
+#define COURSE_H
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class Course
+{
+private:
+    string courseCode;
+    string courseName;
+    string semester;
+    int creditHours;
+    string department;
+    bool isActive;
+
+public:
+    Course();
+
+    ~Course();
+    void input();
+    void display() const;
+    void saveToCSV(std::ofstream &outputFile);
+    void loadFromCSV(std::ifstream &inputFile);
+    void setData(
+
+        string courseCode,
+        string courseName,
+        string semester,
+        int credithours,
+        string department,
+        bool status);
+
+    void setCourseCode(string courseCode);
+    void setCourseName(string courseName);
+    void setSemester(string semester);
+    void setCourseCreditHours(int creditHours);
+    void setDepartment(string department);
+    void setStatus(bool status);
+
+    string getCourseCode() const;
+    string getCourseName() const;
+    string getSemester() const;
+    int getCreditHours() const;
+    void setDepartment() const;
+    bool getStatus() const;
+    
+    bool matchesCode(string searchCode) const;
+};
+#endif
