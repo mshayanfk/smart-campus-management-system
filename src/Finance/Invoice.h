128c7b57c7f43335a2c20d831bc9fa852be8a9e5
Shayan <mshayanfkwb5@gmail.com>
2026-06-05 19:25:46 +0500

    Mainly working on the Fee Receipt Program here

---
+++ src/Finance/Invoice.h
@@ -0,0 +1,48 @@
+#ifndef INVOICE_H
+#define INVOICE_H
+
+#include "FeeRecord.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class Invoice {
+    private:
+
+    int invoiceID;
+    string issueDate;
+    FeeRecord feeRecord;
+    double totalAmount;
+    double paidAmount;
+    double remainingAmount;
+
+    public:
+    Invoice();
+
+    ~Invoice();
+
+    void input();
+    void display() const;
+
+    void generateInvoice(const FeeRecord &record);
+
+    void saveInCSV(ofstream &csvOutputFile) const;
+    void loadFromCSV(ifstream &csvInputFile);
+
+    void printReceipt() const;
+
+    void setInvoiceID(int invoiceID);
+    void setIssuDate(string issueDate);
+    void setFeeRecord(FeeRecord record);
+    void setAmount(double paidAmoutn);
+
+    int getInvoiceID() const;
+    string getIssueDate() const;
+    FeeRecord getFeeRecord() const;
+    double getTotalAmount() const;
+    double getPaidAmount() const;
+    double getRemainingAmount() const;
+};
+
+#endif
