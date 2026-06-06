4f68cfb3fc22e37c76ea6b80ef7c388d0ca7dba1
Spoof <aamir.jailbreak@gmail.com>
2026-06-06 11:45:56 +0500

    Book.h  done

---
+++ src/Library/Book.h
@@ -0,0 +1,61 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+#include "LibraryItem.h"
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class Book : public LibraryItem
+{
+
+private:
+    string author;
+    string isbn;
+    int totalPages;
+    string edition;
+
+public:
+    Book();
+
+    ~Book();
+
+    void input();
+
+    void display() const;
+
+    void saveToCSV(std::ofstream &outputFile);
+
+    void loadFromCSV(std::ifstream &inputFile);
+
+    void setData(
+        string author,
+        string isbn,
+        int totalPages,
+        string edition);
+
+    void setAuthor(string author);
+
+    void setISBN(string isbn);
+
+    void setTotalPages(int totalPages);
+
+    void setEdition(string edition);
+
+    string getAuthor() const;
+
+    string getISBN() const;
+
+    int getTotalPages() const;
+
+    string getEdition() const;
+
+    bool matchesAuthor(string searchAuthor) const;
+
+    bool matchesISBN(string searchISBN) const;
+};
+
+#endif
