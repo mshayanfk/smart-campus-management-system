d463788f4bc824c04bea794c9ec6eb56f4a58504
Shayan <mshayanfkwb5@gmail.com>
2026-06-19 18:36:43 +0500

    Saving and Loading Files Done in the Main.coo feature modules

--- src/main.cpp
+++ src/main.cpp
@@ -76,5 +76,262 @@ void clearScreen() {
 void header(string title) {
     cout << YELLOW << "===========================================" << RESET << endl;
     cout << BOLD << CYAN << " " << title << RESET << endl;
+    cout << YELLOW << "===========================================" << RESET << endl;
 }
 
+void pause() {
+    cout << " " << GREEN << "Press Enter to Continue...." << RESET;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.get();
+}
+
+int getMenuChoice() {
+    int choice;
+    cout << BOLD << GREEN << "Enter your Selection = " << RESET;
+    cin >> choice;
+
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return -1;
+    }
+    return choice;
+}
+
+// Loading all the data from the CSV Files
+
+// Loading the Courses and Enrollment Files
+void loadCourses() {
+    ifstream file(COURSE_FILE);
+    if(!file.is_open()) return;
+    while(file.peek() != EOF && file.good()) {
+        Course c;
+        c.loadFromCSV(file);
+        if(!file.fail()) {
+            courses.push_back(c);
+        }
+    }
+}
+
+void loadEnrollments() {
+    ifstream file(ENROLLMENT_FILE);
+
+    if(!file.is_open()) return;
+    while(file.peek() != EOF && file.good()) {
+        Enrollment e;
+        e.loadFromCSV(file);
+        if (!file.fail()) {
+            enrollments.push_back(e);
+        }
+    }
+}
+
+// Loading All the Person files including Student, Staff and Faculty
+
+void loadPersons() {
+    for(Person* p : persons) delete p;
+    persons.clear();
+
+    ifstream sFile(STUDENT_FILE);
+    if (sFile.is_open()) {
+    while(sFile.peek() != EOF && sFile.good()) {
+        Student* s = new Student();
+        s->loadFromCSV(sFile);
+
+        if (!sFile.fail()) persons.push_back(s);
+        else delete s;
+       }
+       sFile.close();
+    }
+
+
+
+    ifstream fFile(FACULTY_FILE);
+    if (fFile.is_open()) {
+        while(fFile.peek() != EOF && fFile.good()) {
+            Faculty* f = new Faculty();
+            f->loadFromCSV(fFile);
+            if (fFile.fail()) persons.push_back(f);
+            else delete f;
+        }
+
+        fFile.close();
+    }
+
+
+    ifstream stFile(STAFF_FILE);
+    if (stFile.is_open()) {
+        while(stFile.peek() != EOF && stFile.good()) {
+            Staff* st = new Staff();
+            st->loadFromCSV(stFile);
+            if (stFile.fail()) persons.push_back(st);
+            else delete st;
+        }
+        stFile.close();
+    }
+}
+
+// Loading the Library Files
+void loadBooks() {
+    ifstream file(BOOK_FILE);
+    if(!file.is_open()) return;
+    while(file.peek() != EOF && file.good()) {
+        Book b;
+        b.loadFromCSV(file);
+        if(!file.fail()) books.push_back(b);
+    }
+}
+
+void loadJournals() {
+    ifstream file(JOURNAL_FILE);
+    if(!file.is_open()) return;
+    while(file.peek() != EOF && file.good()) {
+        Journal j;
+        j.loadFromCSV(file);
+        if (!file.fail()) journals.push_back(j);
+    }
+}
+
+// Loading Hostel Files
+
+void loadHostelBlocks() {
+    ifstream file(HOSTEL_FILE);
+    if(!file.is_open()) return;
+    while(file.peek() != EOF && file.good()) {
+        HostelBlock block;
+        block.loadFromCSV(file);
+        if(!file.fail()) hostelManager.addBlock(block);
+    }
+}
+
+// Loading Fees & Invoices
+
+void loadFees() {
+    ifstream file(FEE_FILE);
+    if(!file.is_open()) return;
+    while(file.peek() != EOF && file.good()) {
+        FeeRecord f;
+        f.loadFromCSV(file);
+        if (!file.fail()) fees.push_back(f);
+    }
+}
+
+void loadInvoices() {
+    ifstream file(INVOICE_FILE);
+    if(!file.is_open()) return;
+    while(file.peek() != EOF && file.good()) {
+        Invoice in;
+        in.loadFromCSV(file);
+        if (!file.fail()) invoices.push_back(in);
+    }
+}
+
+void loadAllData() {
+    loadCourses();
+    loadEnrollments();
+    loadPersons();
+    loadBooks();
+    loadJournals();
+    loadHostelBlocks();
+    loadFees();
+    loadInvoices();
+}
+
+// Now Saving all the Data
+// Saving the Course Files
+
+void saveCourses() {
+    ofstream file(COURSE_FILE);
+    for(Course &c : courses)
+    c.saveToCSV(file);
+}
+
+void saveEnrollments() {
+    ofstream file(ENROLLMENT_FILE);
+    for(Enrollment &e : enrollments)
+    e.saveToCSV(file);
+}
+
+// For the Person (Staff, Student, Faculty ) ones
+
+void savePersons() {
+    ofstream sFile(STUDENT_FILE);
+    ofstream stFile(STAFF_FILE);
+    ofstream fFile(FACULTY_FILE);
+
+    for (Person* p : persons) {
+        if (!p) continue;
+
+        Student* s = dynamic_cast<Student*>(p);
+        if (s) {
+            s->saveInCSV(sFile);
+            continue;
+        }
+
+        Faculty* f = dynamic_cast<Faculty*>(p);
+        if (f) {
+            s->saveInCSV(fFile);
+            continue;
+        }
+
+        Staff * st = dynamic_cast<Staff *>(p);
+        if (st) {
+            st->saveInCSV(stFile);
+            continue;
+        }
+    }
+
+}
+
+// For Books
+
+void saveBooks() {
+    ofstream file(BOOK_FILE);
+    for(Book &b : books)
+    b.saveToCSV(file);
+}
+
+void saveJournals() {
+    ofstream file(JOURNAL_FILE);
+    for (Journal &j : journals)
+    j.saveToCSV(file);
+}
+
+// For Hostel
+
+void saveHostelBlocks() {
+    ofstream file(HOSTEL_FILE);
+    for(HostelBlock &block : hostelManager.getBlocks()) {
+        block.saveInCSV(file);
+    }
+}
+
+// For Fee Records 
+void saveFees() {
+    ofstream file(FEE_FILE);
+    for (FeeRecord &f : fees)
+    f.saveInCSV(file);
+}
+
+void saveInvoices() {
+    ofstream file(INVOICE_FILE);
+    for (Invoice &in : invoices)
+    in.saveInCSV(file);
+}
+
+void saveAllData() {
+    saveCourses();
+    saveEnrollments();
+    savePersons();
+    saveBooks();
+    saveJournals();
+    saveHostelBlocks();
+    saveFees();
+    saveInvoices();
+}
+ 
+void cleanPersons() {
+    for(Person*p : persons)
+    delete p;
+    persons.clear();
+}
